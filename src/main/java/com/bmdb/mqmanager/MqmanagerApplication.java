032179c54e0d5555e641dbfd09cdd95d623f9fe6
Marcotts <ccdsergio@gmail.com>
2025-07-05 15:08:21 -0400

    Initial commit

---
+++ src/main/java/com/bmdb/mqmanager/MqmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.bmdb.mqmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmdb/mqmanager/MqmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmdb.mqmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
